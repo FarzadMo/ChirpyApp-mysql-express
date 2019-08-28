4858fee4a76af9ff90cac2c58530ec52296172ff
Fred M <farzad.moghbel@gmail.com>
2019-08-28 16:17:05 -0400

    The server is now connected

---
+++ app/config/connection.js
@@ -0,0 +1,16 @@
+var mysql = require ("mysql");
+
+var connection = mysql.createConnection({
+    port: 3306,
+    user: "root",
+    password: "farzad1365",
+    host: "localhost",
+    database: "chirpy"
+});
+
+connection.connect(function(err){
+    if(err) {
+        console.log("The error is"+err.stack);
+    } 
+    console.log("connection is connected as" +connection.threadId);
+})
